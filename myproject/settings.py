0c0fc7a05225d95ebc33eb7ecfbb7fd0b6f82501
Nish-a25 <nishavrottigawad@gmail.com>
2026-07-28 15:42:06 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-key-for-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =Flase
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
