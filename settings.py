96653048e275b0563fee398d865c1a539e9444e5
Nish-a25 <nishavrottigawad@gmail.com>
2026-07-28 15:05:57 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-key-for-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Flase
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
